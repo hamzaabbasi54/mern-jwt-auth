54ac3a8b73a2cf75b874499664e2fa4fa09acdf4
hamzaabbasi54 <hamza.abbasi3030@gmail.com>
2025-10-23 10:58:00 -0700

    created login logout controllers

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -35,3 +35,42 @@ export const register = async(req,res)=>{
         return res.status(500).json({message:"Internal server error"});
     }
 }
+
+export const login = async (req,res)=> {
+    const {email, password} = req.body;
+    if (!email || !password) {
+        return res.status(400).json({message: "fields are missing"});
+    }
+    try {
+        const user = await userModel.findOne({email});
+        if (!user) {
+            return res.status(400).json({message: "Invalid credentials"});
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({message: "Invalid credentials"});
+        }
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: "1d"});
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days in milli seconds
+        });
+        return res.status(200).json({message: "Login successful"});
+    } catch (error) {
+        return res.status(500).json({message: "Internal server error"});
+    }
+}
+
+export const logout = async (req, res) => {
+    try{
+        res.clearCookie('token',{  httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",});
+        return res.status(200).json({message:"Logout successful"});
+    }
+    catch(err){
+        return res.status(500).json({message:"Internal server error"});
+    }
+}
